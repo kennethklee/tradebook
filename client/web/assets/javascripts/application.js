9de54e92da5027bfe9fa1b0401a47a6cad05df40
unknown <Kenneth@.(none)>
2012-07-01 21:56:15 -0400

    Added reset.css and some preliminary javascript

---
+++ client/web/assets/javascripts/application.js
@@ -0,0 +1,8 @@
+$(document).bind('pageinit', function() {
+  setTimeout(function() {
+    $.mobile.showPageLoadingMsg("a", "Please wait...", true);
+  }, 100);
+  setTimeout(function() {
+    $.mobile.hidePageLoadingMsg();
+  }, 1100);
+});
